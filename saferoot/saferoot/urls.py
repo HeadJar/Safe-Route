37c7e6fb6ed3ee3cbc0738a8a8aa1f044d7b5b14
Teek <34976984+TeeKKar@users.noreply.github.com>
2019-11-10 03:02:31 -0500

    Added Saferoot files

--- saferoot/saferoot/urls.py
+++ saferoot/saferoot/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from home.views import post_index, get_index
@@ -21,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', get_index),
     path('searchad/', post_index),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
